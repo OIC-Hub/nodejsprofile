534b675a5f9efe4e9b1e9264738e7c2d6f0a6975
Sunday Omolewu <omolewu12@gmail.com>
2023-02-14 14:07:52 +0100

    completion of CRUD

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,13 @@
  const fs = require('fs');
  const datapath = path.join(__dirname, '..', 'data', 'users.json');
  router.get('/admin', (req, res)=>{
-       res.render('admin/index');
+  fs.readFile(datapath, (err, data)=>{
+    if(!err){
+   let fileData= JSON.parse(data)
+   res.render('admin/index', {users:fileData});
+    }
+  })
+      
  })
  router.get('/add-user', (req, res)=>{
     res.render('admin/add-user');
@@ -28,4 +34,49 @@
       })
     
  })
+ router.post('/admin-delete-user', (req, res)=>{
+  let id =req.body.userId;
+  fs.readFile(datapath, (err, data)=>{
+     let filedata = JSON.parse(data);
+    const newData= filedata.filter((value, index)=>{
+      return value.id  !=id
+    })
+     fs.writeFile(datapath, JSON.stringify(newData), err=>{
+      if(err) throw err
+      res.redirect('/admin')
+     } )
+  });
+ })
+
+ router.get('/admin-update-user/:id', (req, res)=>{
+       let id= req.params.id;
+       fs.readFile(datapath, (err, data)=>{
+        let filedata = JSON.parse(data);
+         let newData = filedata.filter((value)=>{
+           return value.id == id
+         });
+        res.render('admin/update-user', {user:newData})
+       })
+ })
+
+ router.post('/update-user', (req, res)=>{
+  let updatedData =req.body
+  fs.readFile(datapath, (err, data)=>{
+ let fildata = JSON.parse(data)
+ let newData =[]
+ for(let i =0; i < fildata.length; i++){
+  if(fildata[i].id == updatedData.id){
+    newData[i] = updatedData
+  }else{
+    newData[i] = fildata[i];
+  }
+ 
+ }
+   fs.writeFile(datapath, JSON.stringify(newData), (err)=>{
+    if(err) throw err
+   })
+   res.redirect('/admin')
+  });
+ })
+
  module.exports = router
